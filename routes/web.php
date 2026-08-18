94b08645d7e555d6cf7289c7bc3be02d850df7f4
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-18 10:20:14 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ use App\Http\Controllers\{
     AdvancePaymentController,
     SaleAdjustmentNoteController,
     SystemAccountMappingController,
+    DispatchTripController,
 };
 
 Auth::routes();
