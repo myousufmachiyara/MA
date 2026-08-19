726a8d98f38095643f31b54b58f5934355053717
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-19 13:27:18 +0500

    Update SalesReportController.php

--- app/Http/Controllers/SalesReportController.php
+++ app/Http/Controllers/SalesReportController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\SaleInvoice;
 use App\Models\SaleInvoiceItem;
+use App\Models\SaleOrder;
 use App\Models\DispatchTrip;
 use App\Models\ChartOfAccounts;
 use Illuminate\Http\Request;
