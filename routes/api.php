e05b548a92f6eccaa98b2d633ca91731eb884492
Yousuf Machiyara <m.yousufmachiyara@gmail.com>
2026-08-29 13:51:19 +0500

    F

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\SaleOrderController as ApiSaleOrderController;
 use App\Http\Controllers\Api\ProductController as ApiProductController;
 use App\Http\Controllers\Api\HomeController as ApiHomeController;
+use App\Http\Controllers\Api\DispatchTripController; // ← this was missing
 
 Route::post('/booker/login', [AuthController::class, 'login']);
 
@@ -24,6 +25,7 @@ Route::middleware(['auth:sanctum', 'active.user'])->prefix('booker')->group(func
     Route::put('/orders/{id}/cancel', [ApiSaleOrderController::class, 'cancel']);
 
     Route::post('/customers', [ApiSaleOrderController::class, 'storeCustomer']);
+
     Route::get('/trips', [DispatchTripController::class, 'index']);
     Route::get('/trips/{id}', [DispatchTripController::class, 'show']);
     Route::post('/trips/{id}/settle', [DispatchTripController::class, 'settle']);
